c4c424e25a393ff4335ab59a118013d10613c501
CLUB-GIT-ENSPD <clubgitenspd13@gmail.com>
2026-02-17 18:50:29 +0100

    Add files via upload

---
+++ src/test/java/com/clubgit/backend/ClubgitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clubgit.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubgitBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
+
